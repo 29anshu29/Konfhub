331c07eed03925950cea996c4c7a9b4b7e932448
29anshu29 <29anshu29@gmail.com>
2024-08-07 17:37:21 +0530

    5

--- src/Components/Event/Event.jsx
+++ src/Components/Event/Event.jsx
@@ -6,15 +6,16 @@ function Event() {
             <div>
 
                 <h1 className='event-heading'>About Event</h1>
-               <div className="paragraph"><p >This is the description of an event. This event was created as an evaluation task for the role of Frontend
+               <div ><p className="paragraph" >This is the description of an event. This event was created as an evaluation task for the role of Frontend
                      Engineer. Are you the one we are looking for? Don't get nervous. Take help if you need. Ask us more questions if you did not understand the problem. You can reach-us out at reachus@konfhub.com..</p>
                      </div> 
             </div>
             <div><img className='eventimage' src="https://dev-media.konfhub.com/resources/2024/June/09/1717971318450-ffa21dd4-3b8d-4b4c-878e-32d640579510.png" alt="" /></div>
-            <div className="youtube-container">
-        <iframe
-          width="760"
-          height="415"
+            <div>
+        <iframe className="youtube-container"
+          width="770"
+          height="520"
+          
           src="https://www.youtube.com/embed/bEM35JDYjrI\" // Replace with your YouTube video URL
           title="YouTube video player"
           frameBorder="0"
@@ -23,7 +24,31 @@ function Event() {
         ></iframe>
       
     </div>
-            <div></div>
+    <div>
+      
+      <table className="data-table">
+        <thead>
+          <tr >
+            <th>A</th>
+            <th>B</th>
+            <th>C</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>A1</td>
+            <td>B1</td>
+            <td>C1</td>
+          </tr>
+          <tr>
+            <td>A2</td>
+            <td>B2</td>
+            <td>C2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+                
 
 
 
